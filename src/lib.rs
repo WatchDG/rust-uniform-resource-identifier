8a1b4568f35223e9b86801255bc8e9de94a72a70
WatchDG <baranov@w3m.co>
2021-07-30 20:52:52 +0300

    add casts between Port and Scheme

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
+#[derive(Debug, PartialEq)]
 pub enum Scheme {
     Http,
     Https,
+}
+
+impl From<Scheme> for Port {
+    fn from(scheme: Scheme) -> Port {
+        match scheme {
+            Scheme::Http => Port(80),
+            Scheme::Https => Port(443),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_scheme {
+    use crate::{Port, Scheme};
+
+    #[test]
+    fn scheme_to_port() {
+        let scheme = Scheme::Http;
+        let port: Port = scheme.into();
+        assert_eq!(port.0, 80);
+    }
 }
 
 pub struct Port(usize);
@@ -11,13 +33,30 @@ impl Port {
     }
 }
 
+impl From<Port> for Scheme {
+    fn from(port: Port) -> Scheme {
+        match port.0 {
+            80 => Scheme::Http,
+            443 => Scheme::Https,
+            _ => panic!("cast port {} to scheme", port.0),
+        }
+    }
+}
+
 #[cfg(test)]
 mod test_port {
-    use crate::Port;
+    use crate::{Port, Scheme};
 
     #[test]
     fn new_port() {
         let port = Port::new(80);
         assert_eq!(port.0, 80);
+    }
+
+    #[test]
+    fn port_to_scheme() {
+        let port = Port::new(80);
+        let scheme: Scheme = port.into();
+        assert_eq!(scheme, Scheme::Http);
     }
 }
